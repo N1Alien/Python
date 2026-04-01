6b84d5bc0aa8beed3be6aac4e9c6070d98f173d1
N1Alien <n1anoobis@gmail.com>
2026-04-01 17:40:08 +0200

    Add solution

---
+++ main.py
@@ -0,0 +1,20 @@
+"""Zadanie1.4.py"""
+
+for i in range(5):
+    print("* " * 10)
+    print(" *" * 10)
+
+print("               ")
+print("----------------")
+print("               ")
+
+for i in range(5):
+    for j in range(2):
+        print("**" * i)
+
+print("               ")
+print("----------------")
+print("               ")
+
+for i in range(6, 0, -1):
+    print("*" * i)
