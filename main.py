ff1b7c65172a9fbc6356a501170684767c3bd890
N1Alien <n1anoobis@gmail.com>
2026-04-09 14:28:17 +0200

    Add solutions

--- main.py
+++ main.py
@@ -1,71 +1,45 @@
-"""Poprawka zadań z poprzednich lekcji."""
+# # Zadanie 1
 
-for i in range(4):
-    for j in range(2):
-        print("**" * (i + 1))
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+cubed = [number ** 3 for number in numbers if number % 2 != 0]
 
+print(cubed)
 
-CHEESE_1 = "roquefort"
-PRICE_1 = 12.50
-AMOUNT_1 = 2
+# # Zadanie 2
 
-CHEESE_2 = "stilton"
-PRICE_2 = 11.24
-AMOUNT_2 = 1
+list_of_numbers = [2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 2, 3, 0, 0]
+zero_ranges = list_of_numbers.copy()
 
-CHEESE_3 = "brie"
-PRICE_3 = 9.30
-AMOUNT_3 = 1
+non_zero_indexes = [idx for idx, x in enumerate(list_of_numbers) if x != 0]
 
-CHEESE_4 = "gouda"
-PRICE_4 = 8.55
-AMOUNT_4 = 1
+# # # # # # # # # # # # # # # # # # # # # # # 
 
-CHEESE_5 = "edam"
-PRICE_5 = 11.00
-AMOUNT_5 = 1
+zero_ranges = []
+for i in range(len(non_zero_indexes)):
+    start = non_zero_indexes[i] +1
 
-CHEESE_6 = "parmezan"
-PRICE_6 = 16.50
-AMOUNT_6 = 3.5
+    if len(non_zero_indexes) == i + 1:
+        end = len(list_of_numbers)
+    else:
+        end = non_zero_indexes[i +1]
 
-CHEESE_7 = "mozzarella"
-PRICE_7 = 14.00
-AMOUNT_7 = 1.3
-
-CHEESE_8 = "czechosłowacki ser z owczego mleka"
-PRICE_8 = 122.32
-AMOUNT_8 = 2.2
-
-LISTEK_MIETOWY = "listki mięty"
-PRICE_MIETOWY = 20.00
-AMOUNT_MIETOWY = 0.2
-
-LIST_OF_CHEESES = (
-    f"Raport z zakupów:\n"
-    f".................................\n"
-    f"{CHEESE_1}, {AMOUNT_1} kg, {PRICE_1:.2f} zł/kg,\n"
-    f"{CHEESE_2}, {AMOUNT_2} kg, {PRICE_2:.2f} zł/kg,\n"
-    f"{CHEESE_3}, {AMOUNT_3} kg, {PRICE_3:.2f} zł/kg,\n"
-    f"{CHEESE_4}, {AMOUNT_4} kg, {PRICE_4:.2f} zł/kg,\n"
-    f"{CHEESE_5}, {AMOUNT_5} kg, {PRICE_5:.2f} zł/kg,\n"
-    f"{CHEESE_6}, {AMOUNT_6} kg, {PRICE_6:.2f} zł/kg,\n"
-    f"{CHEESE_7}, {AMOUNT_7} kg, {PRICE_7:.2f} zł/kg,\n"
-    f"{CHEESE_8}, {AMOUNT_8} kg, {PRICE_8:.2f} zł/kg,\n"
-    f"{LISTEK_MIETOWY}, {AMOUNT_MIETOWY} kg, {PRICE_MIETOWY:.2f} zł.\n"
-    f".................................\n"
-    f"Suma zł: {
-        (AMOUNT_1 * PRICE_1)
-        + (AMOUNT_2 * PRICE_2)
-        + (AMOUNT_3 * PRICE_3)
-        + (AMOUNT_4 * PRICE_4)
-        + (AMOUNT_5 * PRICE_5)
-        + (AMOUNT_6 * PRICE_6)
-        + (AMOUNT_7 * PRICE_7)
-        + (AMOUNT_8 * PRICE_8)
-        + (PRICE_MIETOWY):.2f}"
-)
+    if start < end:
+        zero_ranges.extend(list_of_numbers[start:end])
 
 
-print(LIST_OF_CHEESES)
+
+print(zero_ranges)
+
+# # # # # # # # # # # # # # # # # # # # # # # 
+
+non_zero_ranges = []
+for i in range(len(non_zero_indexes)):
+    start = non_zero_indexes[i]
+    end = non_zero_indexes[i] +1
+    if start < end:
+        non_zero_ranges.extend(list_of_numbers[start:end])
+
+
+
+print(non_zero_ranges)
