e97b7bec80ed1b356a98cf1e47c8bec7fa616766
N1Alien <72747807+N1Alien@users.noreply.github.com>
2026-04-16 13:03:01 +0200

    Refactor cubed calculation and zero range extraction

--- main.py
+++ main.py
@@ -2,44 +2,29 @@
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-cubed = [number ** 3 for number in numbers if number % 2 != 0]
+cubed = [number ** 3 for number in numbers]
 
-print(cubed)
+for item in cubed:
+    if item % 2 != 0:
+        print(item)
+
 
 # # Zadanie 2
 
 list_of_numbers = [2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 2, 3, 0, 0]
-zero_ranges = list_of_numbers.copy()
 
 non_zero_indexes = [idx for idx, x in enumerate(list_of_numbers) if x != 0]
 
-# # # # # # # # # # # # # # # # # # # # # # # 
 
 zero_ranges = []
-for i in range(len(non_zero_indexes)):
-    start = non_zero_indexes[i] +1
-
-    if len(non_zero_indexes) == i + 1:
-        end = len(list_of_numbers)
-    else:
-        end = non_zero_indexes[i +1]
-
-    if start < end:
-        zero_ranges.extend(list_of_numbers[start:end])
-
-
+for i in range(len(non_zero_indexes) - 1):
+    zero_ranges.extend(list_of_numbers[non_zero_indexes[i] + 1:non_zero_indexes[i + 1]])
+zero_ranges.extend(list_of_numbers[non_zero_indexes[-1] + 1:])
 
 print(zero_ranges)
 
-# # # # # # # # # # # # # # # # # # # # # # # 
-
-non_zero_ranges = []
-for i in range(len(non_zero_indexes)):
-    start = non_zero_indexes[i]
-    end = non_zero_indexes[i] +1
-    if start < end:
-        non_zero_ranges.extend(list_of_numbers[start:end])
 
 
+non_zero_ranges = [list_of_numbers[idx] for idx in non_zero_indexes]
 
 print(non_zero_ranges)
